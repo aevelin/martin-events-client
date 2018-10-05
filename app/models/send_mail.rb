1ea07ec15933e52f46fa741d3dfcd08ca3710eb5
Evelin Arust <evelin.arust@khk.ee>
2018-10-05 14:43:22 +0300

    All you need to send email-vol2

--- app/models/send_mail.rb
+++ app/models/send_mail.rb
@@ -6,7 +6,7 @@ class SendMail
    content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
    mail = SendGrid::Mail.new(from, subject, to, content)
   
-   sg = SendGrid::API.new(api_key: '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')
+   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
    response = sg.client.mail._('send').post(request_body: mail.to_json)
  end
 end
